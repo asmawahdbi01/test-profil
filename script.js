130ed4bb16a9b9817c7e9311e20604f5fe8aeffd
ibrataha8 <115188113+ibrataha8@users.noreply.github.com>
2023-03-13 17:05:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 console.log("Hello, world!");
 let name = prompt("What is your name?");
 alert(`Hello, ${name}! Nice to meet you.`);
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+let guess = parseInt(prompt("I'm thinking of a number between 1 and 10. Can you guess what it is?"));
+
+if (guess === randomNumber) {
+  alert("Congratulations! You guessed the number!");
+} else {
+  alert(`Sorry, the number was ${randomNumber}. Better luck next time!`);
+}
 
